a0ece6ed526859b88d2d4b0ef1643a09dd8eed32
Shubham Kushwah <shubhamkushwaha621@gmail.com>
2021-06-23 16:49:55 +0530

    Added nodemon

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,5 +1,3 @@
-
-
 var myapp = angular.module('myapp', []);
 
 (function (app) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Not express' });
+  res.render('index', { title: 'MEAN App For Learning' });
 });
 
 /**
